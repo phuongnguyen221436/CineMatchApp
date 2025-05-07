63ec71d3169ec6fd8992d688d444ada8903dcd13
pnguye92 <pnguye92@uwo.ca>
2025-05-06 23:40:52 -0400

    Initial Commit

---
+++ CineMatchApp/CineMatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineMatchAppApp.swift
+//  CineMatchApp
+//
+//  Created by Brielle on 2025-05-06.
+//
+
+import SwiftUI
+
+@main
+struct CineMatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
